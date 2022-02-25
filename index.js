ad3e768395cb03acaed6561f7da4058079a5686f
pepe lopez <plopez741@gmail.com>
2022-02-24 22:32:25 -0800

    commit tree 09530c1ed68f3a934652cfc6cd805b4470d8e884

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    const newArray = [];
+    for(let i = 0; i < array.length; i++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray
+}
+function countDown(num){
+    let i = num
+    while(i > (-1)){
+        console.log(i);
+        i--;
+    }
+}
